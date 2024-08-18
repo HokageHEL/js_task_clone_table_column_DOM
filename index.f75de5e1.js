4b871c3a26fc5a445f988aec135c55220764b96e
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-08-18 21:02:02 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = table.querySelectorAll("tr");
+rows.forEach((row)=>{
+    const cells = row.children;
+    const clonedCell = cells[1].cloneNode(true);
+    row.insertBefore(clonedCell, cells[cells.length - 1]);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
